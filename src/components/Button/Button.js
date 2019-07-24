6e5a00a183fecb68cabc588f425a249870e61f38
Allie <allie_michell@hotmail.com>
2019-07-23 23:20:39 -0500

    Create project

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Button from '@material-ui/core/Button'; 
+
+function Button() {
+    return(
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    );
+}
+
+export default Button;
